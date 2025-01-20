c2fb9bde6e0e203172698cdbd129bd3b5d005ac2
pawan <pawan@DESKTOP-G95RVPH>
2025-01-20 22:25:32 +0530

    DSA practices

--- JavaPractice/src/arrays/RotateArray.java
+++ JavaPractice/src/arrays/RotateArray.java
@@ -6,25 +6,28 @@ public class RotateArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int nums[] = {-1,-100,3,99};
+		int nums[] = { -1, -100, 3, 99 };
 		PrintArray pa = new PrintArray();
 		rotate(nums, 2);
 		pa.printSimpleImtArray(nums);
 	}
-    public static void rotate(int[] nums, int k) {
-        int right = nums.length-(k+1);//3
-        int last = nums[right];//4
-        System.out.println(right+","+last);
-        for(int i=0;i<right;i++){//[0,2]
-            swap(i,right,nums);
-        }
-        nums[nums.length-1] = last;
-    }
 
-    static void swap(int i, int right, int nums[]){
-        int temp = nums[i];//1
-        nums[i] = nums[right+i+1];//[0]=5
-        nums[right+i] = temp;//[3]=1
-    }
+	public static void rotate(int[] nums, int k) {
+		int n = nums.length;
+		k %= n;
+		reverse(nums, 0, n - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, n - 1);
+	}
+
+	public static void reverse(int[]nums,int start,int end){
+    	while(start<end){
+    	int temp=nums[start];
+    	nums[start]=nums[end];
+    	nums[end]=temp;
+    	start++;
+    	end--;
+    	}
+	}
 
 }
